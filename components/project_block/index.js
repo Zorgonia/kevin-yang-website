d79b70935820f6eb47329f01731cb6544f784a94
Kevin Yang <kevy.yang@mail.utoronto.ca>
2021-12-26 13:25:08 -0500

    first pass on project block

---
+++ components/project_block/index.js
@@ -0,0 +1,28 @@
+import styles from "./project_block.module.css"
+import Link from "next/link"
+import Image from "next/image"
+import utilStyles from '../../styles/utils.module.css'
+
+export default function ProjectBlock(props) {
+    return (
+        <div className={styles.container}>
+                <Image
+                    src="/images/cc.png"
+                    className={styles.subImage}
+                    height={160}
+                    width={160}
+                    alt={props.title}
+                    layout={"fixed"}
+                />
+
+                <div className={styles.textContainer}>
+                    <p className={styles.titleText}><a href={props.link} className={styles.linkText} target="_blank">{props.title}</a></p>
+                    {/* <p className={styles.titleStyle}>{props.title}</p> */}
+                    
+                    <div 
+                    className={styles.descriptionText} 
+                    dangerouslySetInnerHTML={{__html: props.descriptionText}}></div>
+                </div>
+        </div>
+    )
+}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '../../components/layout/layout'
+import ProjectBlock from '../../components/project_block'
+
+const PROJECT_TEXTS = ["A 2.5D Action game where you're a robot trying to escape the mind with conflicting controls. Can you leverage your partners to make your way through the labryinth? Developed by a team of 3 developers (including myself), 2 artists, and 2 musicians. <br/><br/> Worked on implementing player abilties, designed some levels, implemented art/music assets, and fixed bugs."]
 
 export default function Projects() {
   return (
     <Layout>
       <h1>Yo</h1>
       <p>what's up dog</p>
+      <ProjectBlock 
+        descriptionText={PROJECT_TEXTS[0]} 
+        title="Cross Control" 
+        link="https://zorgonia.itch.io/crosscontrol"/>
       <h2>
         <Link href="/">
             <a>Back to home</a>
