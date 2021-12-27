3ed4eba10b76f1fa5a282e8e8144ca2c1231ec46
Kevin Yang <kevy.yang@mail.utoronto.ca>
2021-12-27 14:58:05 -0500

    trying to fix apostrophes for deployment again

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
   return (
     <Layout>
         <p className={styles.description}>
-            Hey! I'm Kevin Yang and welcome to my website.
+            {"Hey! I\'m Kevin Yang and welcome to my website."}
         </p>
         <p className={styles.subdesc}>
-            I'm a 4th year computer science student currently studying at the University of Toronto. I'm expecting to graduate in June 2022. 
+            {"I\'m a 4th year computer science student currently studying at the University of Toronto. I\'m expecting to graduate in June 2022."} 
             <br/><br/>
-            I'm currently looking for an entry-level software developer position. I'm mainly looking in Toronto, Ontario, but am also looking for positions located in other parts of Canada or the US.
+            {"I\'m currently looking for an entry-level software developer position. I\'m mainly looking in Toronto, Ontario, but am also looking for positions located in other parts of Canada or the US."}
             <br/><br/>
             I am most proficient in <b>Python</b>, and <b>Kotlin</b>, previously having done Android development for a year as a co-op at Accedo. 
             I also have used <b>Java, React, C, </b> and <b>Unity</b> previously. 
